09b4b257aa94711df602826ee796f6e97b073ddf
VadimSel <selyaaa33@gmail.com>
2024-07-09 20:03:15 +0300

    hw14 is done

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW14.module.css'
 import axios from 'axios'
 import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
-import {useSearchParams} from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 /*
 * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
@@ -17,7 +17,7 @@ const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
             'https://samurai.it-incubator.io/api/3.0/homework/test2',
-            {params: {find}}
+            { params: { find } }
         )
         .catch((e) => {
             alert(e.response?.data?.errorText || e.message)
@@ -39,6 +39,9 @@ const HW14 = () => {
                 // сохранить пришедшие данные
 
                 //
+                if (res)
+                    setTechs(res.data.techs)
+                setLoading(false)
             })
     }
 
@@ -48,7 +51,7 @@ const HW14 = () => {
 
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
+        setSearchParams({ find: value })
         //
     }
 
@@ -77,7 +80,7 @@ const HW14 = () => {
                 />
 
                 <div id={'hw14-loading'} className={s.loading}>
-                    {isLoading ? '...ищем' : <br/>}
+                    {isLoading ? '...ищем' : <br />}
                 </div>
 
                 {mappedTechs}
